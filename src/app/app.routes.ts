e5eb522ce6bc56c37a412257784e9c83910d9b3a
ANICA Sean <sean.anica@soprasteria.com>
2024-11-24 19:52:23 +0100

    feat: routes and header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { JobListComponent } from './components/job-list/job-list.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
+import { FavoritesJobsListComponent } from './components/favorites-jobs-list/favorites-jobs-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/jobs', pathMatch: 'full' }, // Redirection vers la liste des jobs par défaut
+  { path: 'jobs', component: JobListComponent }, // Liste des tous les jobs
+  { path: 'favorites', component: FavoritesJobsListComponent }, // Liste des jobs préférés
+  { path: 'jobs/:jobId', component: JobDetailsComponent }, // Détails d'un job
+];
